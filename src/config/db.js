c6cc5251cb8920f9689f897357fe76b6606272b2
Muhammad Ahmad <hafizahmed420@gmail.com>
2021-12-13 19:32:36 +0500

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -4,11 +4,12 @@ const {Sequelize} = require('sequelize');
 
 // const db = {};
 
-const sequelize = new Sequelize('dctid6srkf62qd', 'ogadhimkuyujkl', 'e27283a2a49b7118d1345b53a43af8a741bf62227c50611714d01f77fd03fafb', {
-    host: 'ec2-54-204-128-96.compute-1.amazonaws.com',
-    
-    logging: false,
-    
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }
 });
 
 module.exports = sequelize;
