3d368bab27467e0fb73471bcaf4fe6f56265f979
Muhammad Ahmad <hafizahmed420@gmail.com>
2021-12-13 18:29:22 +0500

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -9,8 +9,6 @@ const sequelize = new Sequelize('dae80ub29525jp', 'yxrqrgiuosbwtb', 'edb19b0dddb
     host: 'ec2-18-214-214-252.compute-1.amazonaws.com',
     dialect: 'postgres',
     logging: false,
-    port: 5432,
-    URI :'postgres://yxrqrgiuosbwtb:edb19b0dddbd94c84a7d6b766d969f64388570fcd75ac88bdb63060d92759635@ec2-18-214-214-252.compute-1.amazonaws.com:5432/dae80ub29525jp',
     pool:{
         max: 5,
         min:0,
