f3f45c51459acc3633bf8f29127528fe93e1480c
Muhammad Ahmad <hafizahmed420@gmail.com>
2021-12-13 18:12:25 +0500

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -5,8 +5,8 @@ dotenv.config();
 
 // const db = {};
 
-const sequelize = new Sequelize('deliveryApp', 'postgres', '12344321', {
-    host: 'localhost',
+const sequelize = new Sequelize('dae80ub29525jp', 'yxrqrgiuosbwtb', 'edb19b0dddbd94c84a7d6b766d969f64388570fcd75ac88bdb63060d92759635', {
+    host: 'ec2-18-214-214-252.compute-1.amazonaws.com',
     dialect: 'postgres',
     logging: false,
     pool:{
