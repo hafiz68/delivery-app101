ac8831c44c9f1ae6e190fcef97275cb540ae2dc9
Muhammad Ahmad <hafizahmed420@gmail.com>
2021-12-14 10:36:20 +0500

    Update mailService.js

--- src/services/mailService.js
+++ src/services/mailService.js
@@ -8,15 +8,27 @@ const sendingmail = async(email, mailText) =>{
             service: 'gmail',
             host: 'smtp.gmail.com',
             auth: {
+              type: 'OAuth2',
               user: process.env.GMAIL_ACC,
               pass: process.env.GMAIL_PASS,
             }
           }));
+          transporter.set('oauth2_provision_cb', (user, renew, callback) => {
+            let accessToken = userTokens[user];
+            if(!accessToken){
+                return callback(new Error('Unknown user'));
+            }else{
+                return callback(null, accessToken);
+            }
+        });
         let mailOptions = {
             from: process.env.GMAIL_ACC,
             to: email,
-            subject: 'Sending Email using Node.js[nodemailer]',
-            text: mailText
+            subject: 'Welcome to our app',
+            text: mailText,
+            auth: {
+              user: process.env.GMAIL_ACC,
+          }
           };
 
         transporter.sendMail(mailOptions, function(error, info){
